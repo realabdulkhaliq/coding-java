3e6a63743f37a5e3fe6332492ae448ae043b86b1
NewAgeDeveloper <thenewagedevelopers@gmail.com>
2024-02-24 21:38:22 +0500

    Truck OOP

---
+++ Assig(1,07,11,26,41)/src/autoMobile/truck.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package autoMobile;
+
+/**
+ *
+ * @author Home
+ */
+public abstract class truck implements Vahicals {
+
+    public void incerasespeed() {
+    }
+    double d;
+}
