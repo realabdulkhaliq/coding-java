167e718180e13c9d1249a911d63a01f576c1a8da
NewAgeDeveloper <thenewagedevelopers@gmail.com>
2024-02-27 17:14:52 +0500

    Assignment

---
+++ Assig(1,07,11,26,41)/src/autoMobile/critrias.java
@@ -0,0 +1,18 @@
+package autoMobile;
+
+public class critrias {
+
+    public double capacity;
+
+    public void setcapacity(int c) {
+        capacity = c;
+    }
+
+    public double getcap() {
+        return capacity;
+    }
+
+    public void style() {
+        String color = "rad";
+    }
+}
