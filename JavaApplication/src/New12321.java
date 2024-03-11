f1756eabc1c507a95fdad3ad17e1a5cc955a26e2
NewAgeDeveloper <thenewagedevelopers@gmail.com>
2024-03-11 21:59:49 +0500

    J2SE

---
+++ JavaApplication/src/New12321.java
@@ -0,0 +1,35 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Abdul Khaliq
+ */
+public class New12321 {
+     public static void main(String[] args){
+        double e , f, g , h  ;
+       /*  String x=JOptionPane.showInputDialog("Enter any number");
+       int a=Integer.parseInt(x);
+       if (a>0 && a<10){ 
+       for( a=0; a<5; a++){
+          e=a*a*a*a*a*a*a*a;
+   f=e*e;
+   g=a*a;
+  h=f/g;
+   h=a/g;*/
+       //   e=555555*555555;
+         g=55555555;
+         e=g*g;
+         f=25;
+         h=e/f;
+       System.out.println(e); 
+       System.out.println(h);   
+   
+   JOptionPane.showMessageDialog(null,""+h);
+}
+       }
