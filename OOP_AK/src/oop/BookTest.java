101dc2e8a6752be53be6a1ad25d396bbc118301e
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-11-02 17:47:16 +0500

    Book Test

---
+++ OOP_AK/src/oop/BookTest.java
@@ -0,0 +1,18 @@
+
+/**
+ *
+ * @author Abdul Khaliq
+ */
+public class BookTest {
+    public static void main(String args[]){
+       // Book test[]=new Book[10];
+       // test[1]=new Book();
+       // test[1].getBookInfo();
+        
+        
+        Book c=new Book();
+       // c=new Book();
+        c.getBookInfo();
+       // System.out.println(""+c.getBookInfo());
+    }
+}
