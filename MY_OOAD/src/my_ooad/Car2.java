8adff5a5b20332c0e4fb88ffc945d42a4cd7d7de
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-11-12 11:12:32 +0500

    Car Class 2

---
+++ MY_OOAD/src/my_ooad/Car2.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my_ooad;
+
+/**
+ *
+ * @author Abdul Khaliq
+ */
+public abstract class Car2 extends Carabs2 implements Vehicle2{
+    
+    
+    void Car2(){
+        
+    }
+    public void speedincrease(){
+    
+    }
+    
+    public void speeddecrease(){
+    
+    }
+    public void seats(int st){
+         seats = st;
+    }
+    
+    public void color(){
+    
+    }
+
+    
+    
+    
+}
