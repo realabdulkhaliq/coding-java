dea20452efc3d5f1f0716f8f25dd4c99bdb1924b
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-11-14 09:29:50 +0500

    Car Abstract class

---
+++ MY_OOAD/src/my_ooad/Carabs2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my_ooad;
+
+/**
+ *
+ * @author Abdul Khaliq
+ */
+public abstract class Carabs2 implements Vehicle2{
+    
+    double speedincrease, speeddecrease;
+    int seats;
+    String color;
+    
+    public void speedincrease(){
+    }
+    
+    public void speeddecrease(){
+    
+    }
+    public void color(){
+        
+    }
+    
+    public void seats(int st){
+        seats=st;
+        
+    }
+    
+}
