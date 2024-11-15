52dbd26a0814d06c59c3c07b2b78745722e0f1fa
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-11-15 17:52:04 +0500

    Car Class

---
+++ MY_OOAD/src/my_ooad/ClassCar1.java
@@ -0,0 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my_ooad;
+
+/**
+ *
+ * @author Abdul Khaliq
+ */
+public class ClassCar1 extends Car1{
+    
+    static double w1, w2, w3;
+    public void name(String n){
+        name=n;
+    }
+    public String getname(){
+        return name;
+    }
+    
+    public void price(double p){
+        price=p;
+    }
+    public double getprice(){
+        return price;
+    }
+    
+    public void speed(double s){
+        speed=s;
+    }
+    public double getspeed(){
+        return speed;
+    }
+    
+    public void capacity(double c){
+        capacity=c;
+    }
+    public double getcapacity(){
+        return capacity;
+    }
+    
+    
+    public void color(String co){
+        color=co;
+    }
+    public String getcolor(){
+        return color;
+    }
+    
+    public void speedincrease(double si){
+        speedincrease=si;
+    }
+    public double getspeedincrease(){
+        return speedincrease;
+    }
+    
+    public void speeddecrease(double sd){
+        speeddecrease=sd;
+    }
+    public double getspeeddecrease(){
+        return speeddecrease;
+    }
+    public void seats(int st){
+         seats = st;
+    }
+    public int getseats(){
+        return seats;
+    }
+
+    
+    
+}
