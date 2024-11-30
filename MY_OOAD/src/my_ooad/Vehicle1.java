ad9112bd1c20ad6fab4459efb40da01a1af064a4
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-11-30 14:32:53 +0500

    Vehical 1 Class

---
+++ MY_OOAD/src/my_ooad/Vehicle1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my_ooad;
+
+/**
+ *
+ * @author Abdul Khaliq
+ */
+public interface Vehicle1 {
+    public void name(String n);
+    
+    public void price();
+    
+    public void speed();
+    
+    public void capacity();
+    
+}
+
