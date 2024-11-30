d11549835dfe622a85d829955e5889a980f06d52
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-11-30 14:33:23 +0500

    Vehical 2 Class

---
+++ MY_OOAD/src/my_ooad/Vehicle2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my_ooad;
+
+/**
+ *
+ * @author Abdul Khaliq
+ */
+public interface Vehicle2 {
+    
+    public void speedincrease();
+    
+    public void speeddecrease();
+    
+    public void seats(int st);
+    
+    public void color();
+    
+}
