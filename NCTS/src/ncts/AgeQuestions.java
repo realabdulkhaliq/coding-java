4bf145449d7ed8919d3ad357b8a9856bbc86e332
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-01-01 11:18:44 +0500

    Age Questions

--- NCTS/src/ncts/AgeQuestions.java
+++ NCTS/src/ncts/AgeQuestions.java
@@ -23,6 +23,6 @@ public class AgeQuestions {
      a1=A*365;a2=a1*24; a3=a2*60; a4=a3*60;
       b1=B*365;b2=b1*24; b3=b2*60; b4=b3*60;
       JOptionPane.showMessageDialog(null,"Age of A is="+A+"years \n"+a1+"days \n"+a2+"hours \n"+a3+"minuts \n"+a4+"seconds" );
-     JOptionPane.showMessageDialog(null,"Age of B is="+B+"years \n"+b1+"days \n"+b2+"hours \n"+b3+"minuts \n"+b4+"seconds" );
+      JOptionPane.showMessageDialog(null,"Age of B is="+B+"years \n"+b1+"days \n"+b2+"hours \n"+b3+"minuts \n"+b4+"seconds" );
    } 
 }
