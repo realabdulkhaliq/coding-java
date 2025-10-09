a1056f8c27c5a8569327309081735e08701d1aa4
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-10-09 06:32:40 +0500

    Letter Grade Example in Java

---
+++ Assignment3/src/Lettergrades.java
@@ -0,0 +1,28 @@
+
+import javax.swing.JOptionPane;
+
+
+public class Lettergrades {
+    public static void main(String []args){
+  char ch1='A';
+  double a1=0;
+       if(ch1=='A')
+           a1=3.7;
+
+   char s1='+';
+     double t1=0;
+     double t2=0;
+     double f;
+  if(s1=='+') {t1=0.3;
+     f=a1+t1;}
+  else
+  {
+          f=a1;
+  }
+
+      System.out.println(+f);
+  }
+}
+
+
+    
