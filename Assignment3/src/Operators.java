f6f833dde35d0d05e372301edc91f53bebebf468
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-10-10 06:42:07 +0500

    Operators in Java

---
+++ Assignment3/src/Operators.java
@@ -0,0 +1,17 @@
+
+import javax.swing.JOptionPane;
+
+
+
+public class Operators {
+    public static void main(String []args){
+        int b;
+        double a=12.5;
+        b=3;
+double c=(((a+b)*(a))/b)%3;
+JOptionPane.showMessageDialog(null,"Calculation is" +c);
+
+
+    }
+
+}
